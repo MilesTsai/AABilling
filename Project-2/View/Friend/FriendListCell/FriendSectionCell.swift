3d02023610e74c729dcf42581eeba5efe7d00fa9
Tsai,Hsien Han <sian91355@gmail.com>
2019-04-23 15:10:03 +0800

    Creat friend invite list

---
+++ Project-2/View/Friend/FriendListCell/FriendSectionCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendSectionCell.swift
+//  Project-2
+//
+//  Created by User on 2019/4/23.
+//  Copyright © 2019 Miles. All rights reserved.
+//
+
+import UIKit
+
+class FriendSectionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
